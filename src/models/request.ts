ebdb2a6b92333ee4d48be4c4d2be5a70c01f2085
George Bull <geo@exemail.com.au>
2023-07-23 17:31:06 +1000

    updated get requests to return more information

--- src/models/request.ts
+++ src/models/request.ts
@@ -14,6 +14,7 @@ export interface Request {
   respondents: Array<ObjectId>;
   requestedOn: Date;
   dateRange: DateRange;
+  additionalInfo: string;
 }
 
 export async function getRequestWithId(requestId: ObjectId) {
@@ -29,6 +30,16 @@ export async function getOwnerRequests(owner: WithId<Owner>) {
   const res = await ownerCollection.aggregate([
     { $match: { _id: owner._id } },
     { $unwind: "$requests" },
+    // add location information into each request
+    {
+      $addFields: {
+        "requests.location": {
+          state: "$location.state",
+          city: "$location.city",
+          street: "$location.street",
+        },
+      },
+    },
     { $replaceWith: "$requests" },
     // add some of the carers info onto the request by joining their information
     // if the carer id is present in the request
@@ -45,6 +56,23 @@ export async function getOwnerRequests(owner: WithId<Owner>) {
           { $project: { _id: 1, name: 1 } },
         ],
         as: "carer",
+      },
+    },
+    // add some of the pet info back into the request
+    {
+      $lookup: {
+        from: "users",
+        let: { pets: "$pets" },
+        pipeline: [
+          {
+            $match: { _id: owner._id },
+          },
+          { $unwind: "$pets" },
+          { $replaceWith: "$pets" },
+          { $match: { $expr: { $in: ["$_id", "$$pets"] } } },
+          { $project: { _id: 1, name: 1, petType: 1 } },
+        ],
+        as: "pets",
       },
     },
     // flatten the carer array if it is present
